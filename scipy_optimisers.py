2c3bf31d5c65933eadb616de463b168622d3f61b
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-07-24 13:19:45 +0100

    Added more optimisers

---
+++ scipy_optimisers.py
@@ -0,0 +1,33 @@
+import numpy as np
+import benchmarker
+import scipy.optimize
+"""
+Solver error for SLSQP
+ArithmeticError: Function CVode() failed with flag -4 CV_CONV_FAILURE: Convergence test failures occurred too many times during one internal time step or minimum step size was reached.
+SimulationError: A numerical error occurred during simulation at t = 0.
+Last reached state: 
+  ikr.act = 0.0
+  ikr.rec = 1.0
+Inputs for binding:
+  time        = 0.0
+  realtime    = 0.0
+  evaluations = 53.0
+  pace = -80.0
+Function CVode() failed with flag -4 CV_CONV_FAILURE: Convergence test failures occurred too many times during one internal time step or minimum step size was reached.
+"""
+
+bm = benchmarker.HH_Benchmarker()
+
+x0 = np.ones(bm.n_parameters())
+
+methods = ['nelder-mead', 'powell', 'CG', 'BFGS', 'Newton-CG', 'L-BFGS-B', 'TNC', 'COBYLA', 'trust-constr'] #trust region methods (except strust-constr) and dogleg require hessian so are ignored, newton cg requires jocobian which is approximated, SLSQP causes solver error - need to investigate
+
+for i in range(len(methods)):
+    bm.reset()
+    print(methods[i])
+    if methods[i] in ['Newton-CG']:
+        out = scipy.optimize.minimize(bm.cost, x0, method=methods[i], options={'disp': True}, jac = lambda x : scipy.optimize.approx_fprime(x, bm.cost, epsilon = 1e-3), bounds = [(0,None)]*bm.n_parameters())
+    else:
+        out = scipy.optimize.minimize(bm.cost, x0, method=methods[i], options={'disp': True}, bounds = [(0,None)]*bm.n_parameters())
+    bm.evaluate(out.x)
+
---
+++ xnes_pints.py
@@ -0,0 +1,48 @@
+import pints
+import benchmarker
+import numpy as np
+
+class Model(pints.ForwardModel):
+    def __init__(self):
+        self.bm = benchmarker.HH_Benchmarker()
+        
+    def n_parameters(self):
+        return self.bm.n_parameters()
+    
+    def simulate(self, parameters, times):
+        # Reset the simulation
+        return self.bm.simulate(parameters, times)
+
+iterCount = 1
+maxIter = 1000
+model = Model()
+problem = pints.SingleOutputProblem(model, np.arange(model.bm.tmax), model.bm.data)
+parameters = np.ones(model.n_parameters())
+error = pints.MeanSquaredError(problem)
+logTransforms = [0, 2, 4, 6]
+
+for i in range(len(parameters)):
+    if i == 0:
+        if i in logTransforms:
+            transformation = pints.LogTransformation(n_parameters=1)
+        else:
+            transformation = pints.IdentityTransformation(n_parameters=1)
+    else:
+        if i in logTransforms:
+            transformation = pints.ComposedTransformation(transformation, pints.LogTransformation(n_parameters=1))
+        else:
+            transformation = pints.ComposedTransformation(transformation, pints.IdentityTransformation(n_parameters=1))
+
+fbest = np.inf
+for i in range(iterCount):
+    x0 = parameters * 2**np.random.normal(0, 0.5, len(parameters))
+    # Create an optimisation controller
+    opt = pints.OptimisationController(error, x0, transformation=transformation, method=pints.XNES)
+    opt.set_max_iterations(maxIter)
+    # Run the optimisation
+    x, f = opt.run()
+    if f<fbest:
+        fbest = f
+        xbest = x
+
+model.bm.evaluate(xbest)
