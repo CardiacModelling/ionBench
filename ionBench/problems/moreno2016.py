67859bb5f33d420fa13d629f1ad2fe2a796535b5
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-08-09 13:38:09 +0100

    Problems with moreno
    Probability leak in .mmt, now fixed
    Choosing tolerances seems to be an issue, heavily dependent on choice but too strict leads to crashing
    Some peaks in the protocols occurred during the 1ms transition between voltages so steps are now 0.1ms
    I have increased the resolution on the measurement windows and made it customisable.
    For some reason there seems to be peaks when going from -100mV to -10mV for SSI but not when going from -100mV to -10mV for the later protocols like tau and rfi

--- ionBench/problems/moreno2016.py
+++ ionBench/problems/moreno2016.py
@@ -15,6 +15,7 @@ class ina(ionBench.benchmarker.Benchmarker):
         self._trueParams = self.defaultParams
         self.loadData(dataPath = os.path.join(ionBench.DATA_DIR, 'moreno2016', 'ina.csv'))
         super().__init__()
+        self.sim.set_tolerance(1e-14,1e-14)
         print('Benchmarker initialised')
     
     def sample(self, width=5):
@@ -31,8 +32,11 @@ class ina(ionBench.benchmarker.Benchmarker):
             else:
                 self.sim.set_constant(self._paramContainer+'.p'+str(i+1), parameters[i])
     
-    def simulate(self, parameters, times):
+    def simulate(self, parameters, times, continueOnError = True):
         #Add parameter error to list
+        for i in range(self.n_parameters()):
+            if self._logTransformParams[i]:
+                parameters[i] = np.exp(parameters[i])
         self._paramRMSE.append(np.sqrt(np.mean((parameters-self._trueParams)**2)))
         self._paramIdentifiedCount.append(np.sum(np.abs(parameters-self._trueParams)<0.05))
         #Simulate the model and find the current
@@ -44,11 +48,10 @@ class ina(ionBench.benchmarker.Benchmarker):
                 return [np.inf]*len(times)
         self.setParams(parameters)
         
-        self.setParams(parameters)
         measurementWindows = []
         #SSI/SSA measurements
         for i in range(9):
-            measurementWindows.append([5001+i*10000, (i+1)*10000])
+            measurementWindows.append([5000+i*10000, 5010+i*10000]) #Assumes peak falls in first 5ms
         #SSA/ACT measurement
         for i in range(20):
             measurementWindows.append([(i+10)*10000])
@@ -56,16 +59,21 @@ class ina(ionBench.benchmarker.Benchmarker):
         dt = [0.1, 0.5, 1, 3, 6, 9, 12, 15, 21, 30, 60, 90, 120, 210, 300, 450, 600, 900, 1500, 3000, 6000]
         for i in range(21):
             #Add first pulse
-            measurementWindows.append([i*5000+295001,i*5000+295001+99])
+            measurementWindows.append([i*5000+295000,i*5000+295000+5]) #Assumes the peak falls in the first 5 ms
             #Add second pulse
-            measurementWindows.append([i*5000+295100+dt[i]+0.01,i*5000+295100+dt[i]+0.01+25])
+            measurementWindows.append([i*5000+295100+dt[i],i*5000+295100+dt[i]+5]) #Assumes the peak falls in the first 5 ms
         #TAU measurements
         for i in range(9):
-            measurementWindows.append([i*5000+405001,i*5000+406000])
+            measurementWindows.append([i*5000+410000,i*5000+410005]) #Assume peak and 50% of peak happen in first 5ms
+        #RDUB measurements
+        startTimes = [500000, 507630.48, 515000, 522779.98, 530000, 538377.98, 545000, 555171.98, 565000, 581450.98, 590000, 624390.98, 630000, 727180.98, 735000, 1011580.98, 1020000, 1924480.98, 1935000, 4633480.98]
+        for i in startTimes:
+            measurementWindows.append([i,i+25])
         logTimes = []
+        windowRes = 100000
         for i in measurementWindows:
             if len(i)==2:
-                logTimes += list(np.linspace(i[0],i[1],num=100)) #100 points per windows
+                logTimes += list(np.linspace(i[0],i[1],num=windowRes)) #100 points per windows
             else:
                 #SSA/ACT protocol
                 logTimes += i
@@ -75,31 +83,72 @@ class ina(ionBench.benchmarker.Benchmarker):
         log = self.sim.run(self.tmax+1, log_times = logTimes, log = [self._outputName])
         #log = self.sim.run(self.tmax+1, log_times = logTimes)
         inaOut = log[self._outputName]
-        ssi = [None]*9
+        inaOut = [-1*a for a in inaOut]
+        ssi = [-1]*9
+        print("")
+        print("----ssi----")
         for i in range(len(ssi)):
-            ssi[i] = max(inaOut[i*100:(i+1)*100])
+            ssi[i] = max(inaOut[i*windowRes:(i+1)*windowRes])
+            print(i)
+            print(ssi[i])
         ssi = [a/max(ssi) for a in ssi]
-        act = [None]*20
+        act = [-1]*20
+        offset = len(ssi)*100
+        print("")
+        print("---act----")
         for i in range(len(act)):
-            act[i] = inaOut[900+i]
+            act[i] = inaOut[offset+i]
+            print(i)
+            print(act[i])
         act = [a/max(act) for a in act]
-        rfi = [None]*21
+        rfi = [-1]*21
+        offset = len(ssi)*windowRes+len(act)
+        print("")
+        print("----rfi----")
         for i in range(len(rfi)):
-            firstPulse = max(inaOut[920+i*200:1020+i*200])
-            secondPulse = max(inaOut[1020+i*200:1120+i*200])
+            firstPulse = max(inaOut[offset+i*200: offset+100+i*200])
+            secondPulse = max(inaOut[offset+100+i*200: offset+200+i*200])
             rfi[i] = secondPulse/firstPulse
-        tau = [None]*9
+            print(i)
+            print(dt[i])
+            print(firstPulse)
+            print(secondPulse)
+            print(rfi[i])
+        tau = [-1]*9
+        offset = (len(ssi)+len(rfi)*2)*windowRes+len(act)
+        print("")
+        print("----tau----")
         for i in range(len(tau)):
-            peak = max(inaOut[5120+i*100:5120+(i+1)*100])
+            peak = max(inaOut[offset+i*100:offset+(i+1)*100])
+            print(i)
+            print(peak)
             reachedPeak = False
             for j in range(100):
-                current = inaOut[5120+i*100+j]
+                current = inaOut[offset+i*100+j]
                 if current == peak:
                     reachedPeak = True
+                    print("reached peak at j:"+str(j))
                 if reachedPeak and abs(current)<=abs(peak/2):
-                    tau[i] = logTimes[5120+i*100+j]-logTimes[5120+i*100]
+                    tau[i] = logTimes[offset+i*100+j]-logTimes[offset+i*100]
+                    print("Reached 50\%")
+                    print(tau[i])
                     break
-        return ssi+act+rfi+tau
+            print("Final current")
+            print(current)
+        rdub = [-1]*(len(startTimes)//2)
+        offset = (len(ssi)+len(rfi)*2+len(tau))*windowRes+len(act)
+        print("")
+        print("----rdub----")
+        for i in range(len(startTimes)//2):
+            firstPulse = max(inaOut[offset+i*200:offset+i*200+100])
+            lastPulse = max(inaOut[offset+i*200+100:offset+i*200+200])
+            rdub[i] = lastPulse/firstPulse
+            print(i)
+            print(firstPulse)
+            print(secondPulse)
+            print(rdub[i])
+        return ssi+act+rfi+tau+rdub
+    #Cost function should be weighted by the number in each group
 
 def generateData():
     bm = ina()
@@ -107,4 +156,3 @@ def generateData():
     with open(os.path.join(ionBench.DATA_DIR, 'moreno2016', 'ina.csv'), 'w', newline = '') as csvfile:
         writer = csv.writer(csvfile, delimiter = ',')
         writer.writerows(map(lambda x: [x], out))
-
