a18e80df68befe12ab125c028986d7c2f883abde
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-08-10 14:15:23 +0100

    Added sampler to staircase problems
    Also combined some common features between the two staircase problems

--- ionBench/problems/staircase.py
+++ ionBench/problems/staircase.py
@@ -4,36 +4,45 @@ import csv
 import myokit
 import ionBench
 
-class HH_Benchmarker(ionBench.benchmarker.Benchmarker):
+class Staircase_Benchmarker(ionBench.benchmarker.Benchmarker):
+    def __init__(self):
+        self._log = myokit.DataLog.load_csv(os.path.join(ionBench.DATA_DIR, 'staircase', 'staircase-ramp.csv'))
+        super().__init__()
+        try:
+            self.loadData(os.path.join(ionBench.DATA_DIR, 'staircase', 'data'+self._modelType+'.csv'), os.path.join(ionBench.DATA_DIR, 'staircase', 'trueParams'+self._modelType+'.csv'))
+        except FileNotFoundError:
+            self.data = None
+            self._trueParams = None
+    
+    def sample(self, n=1):
+        params = [None]*n
+        for i in range(n):
+            params[i] = np.random.uniform(0.5,1.5,self.n_parameters())
+        if n==1:
+            return params[0]
+        else:
+            return params
+        
+class HH_Benchmarker(Staircase_Benchmarker):
     def __init__(self):
         print('Initialising Hodgkin-Huxley IKr benchmark')
         self.model = myokit.load_model(os.path.join(ionBench.DATA_DIR, 'staircase', 'beattie-2017-ikr-hh.mmt'))
-        self._log = myokit.DataLog.load_csv(os.path.join(ionBench.DATA_DIR, 'staircase', 'staircase-ramp.csv'))
         self._outputName = 'ikr.IKr'
         self._paramContainer = 'ikr'
+        self._modelType = 'HH'
         self.defaultParams = [2.26e-4, 0.0699, 3.45e-5, 0.05462, 0.0873, 8.91e-3, 5.15e-3, 0.03158, 0.1524]
         super().__init__()
-        try:
-            self.loadData(os.path.join(ionBench.DATA_DIR, 'staircase', 'dataHH.csv'), os.path.join(ionBench.DATA_DIR, 'staircase', 'trueParamsHH.csv'))
-        except FileNotFoundError:
-            self.data = None
-            self._trueParams = None
         print('Benchmarker initialised')
 
-class MM_Benchmarker(ionBench.benchmarker.Benchmarker):
+class MM_Benchmarker(Staircase_Benchmarker):
     def __init__(self):
         print('Initialising Markov Model IKr benchmark')
         self.model = myokit.load_model(os.path.join(ionBench.DATA_DIR, 'staircase', 'fink-2008-ikr-mm.mmt'))
-        self._log = myokit.DataLog.load_csv(os.path.join(ionBench.DATA_DIR, 'staircase', 'staircase-ramp.csv'))
         self._outputName = 'IKr.i_Kr'
         self._paramContainer = 'iKr_Markov'
+        self._modelType = 'MM'
         self.defaultParams = [0.20618, 0.0112, 0.04209, 0.02202, 0.0365, 0.41811, 0.0223, 0.13279, -0.0603, 0.08094, 0.0002262, -0.0399, 0.04150, -0.0312]
         super().__init__()
-        try:
-            self.loadData(os.path.join(ionBench.DATA_DIR, 'staircase', 'dataMM.csv'), os.path.join(ionBench.DATA_DIR, 'staircase', 'trueParamsMM.csv'))
-        except FileNotFoundError:
-            self.data = None
-            self._trueParams = None
         print('Benchmarker initialised')
 
 def generateData(modelType):
