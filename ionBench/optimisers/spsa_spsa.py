150bed093e3e3f8af74a5bacc07ad371ff54a96d
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-08-11 10:43:54 +0100

    SPSA docstring

--- ionBench/optimisers/spsa_spsa.py
+++ ionBench/optimisers/spsa_spsa.py
@@ -3,7 +3,27 @@ import spsa
 #To do:
 #Convergence needs to massively improve. An implementation of the original Spall 1992 algorithm might actually help
 
-def run(bm, x0, printFrequency=100, maxiter = 100):
+def run(bm, x0, printFrequency=100, maxiter = 1000):
+    """
+    Runs SPSA (Simultaneous Perturbation Stochastic Approximation) from Pints using a benchmarker. 
+
+    Parameters
+    ----------
+    bm : Benchmarker
+        A benchmarker to evaluate the performance of the optimisation algorithm.
+    x0 : list
+        Initial parameter vector from which to start optimisation.
+    printFrequency : int, optional
+        Number of iterations to wait between printing output
+    maxIter : int, optional
+        Number of iterations of SPSA to run per repeat. The default is 1000.
+
+    Returns
+    -------
+    xbest : list
+        The best parameters identified by SPSA.
+
+    """
     counter = 0
     for variables in spsa.iterator.minimize(spsa.with_input_noise(bm.cost, shape=bm.n_parameters(), noise = 0.01), x0, adam=False):
         if counter % printFrequency == 0:
