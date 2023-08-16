c5eccea22e5625068be2ff53607def7f504a390d
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-08-16 14:11:40 +0100

    Added hybrid Loewe 2016 optimiser
    Added the PSO TRR hybrid optimiser from Loewe et al 2016

--- ionBench/optimisers/external_optimisers/__init__.py
+++ ionBench/optimisers/external_optimisers/__init__.py
@@ -2,3 +2,4 @@ from ionBench.optimisers.external_optimisers import GA_Bot2012
 from ionBench.optimisers.external_optimisers import GA_Smirnov2020
 from ionBench.optimisers.external_optimisers import patternSearch_Kohjitani2022
 from ionBench.optimisers.external_optimisers import ppso_chen2012
+from ionBench.optimisers.external_optimisers import hybridPSOTRR_Loewe2016
