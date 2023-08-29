Bug fix?: Overflow appearing in moreno
Might be caused by using lists and numpy arrays in cost subtraction.
Didn't seem to always happen. Might be fixed from casting to numpy arrays. Following warnings were reported:
<string>:43: RuntimeWarning: overflow encountered in exp
<string>:45: RuntimeWarning: overflow encountered in power
<string>:43: RuntimeWarning: invalid value encountered in multiply
C:\Users/pmzmo/OneDrive - The University of Nottingham/Documents/Python/ionBench\ionbench\benchmarker\benchmarker.py:436: RuntimeWarning: overflow encountered in square
  self.tracker.update(parameters, cost = np.sqrt(np.mean((out-self.data)**2)), incrementSolveCounter = incrementSolveCounter)
C:\Users/pmzmo/OneDrive - The University of Nottingham/Documents/Python/ionBench\ionbench\benchmarker\benchmarker.py:312: RuntimeWarning: overflow encountered in square
  cost = np.sqrt(np.mean((testOutput-self.data)**2))

ionbench/benchmarker/benchmarker.py
@@ -388,13 +388,13 @@ class Benchmarker():
         if continueOnError:
             try:
                 log = self.sim.run(self.tmax+1, log_times = times)
-                return log[self._outputName]
+                return np.array(log[self._outputName])
             except:
                 warnings.warn("Failed to solve model. Will report infinite output in the hope of continuing the run.")
-                return [np.inf]*len(times)
+                return np.array([np.inf]*len(times))
         else:
             log = self.sim.run(self.tmax+1, log_times = times)
-            return log[self._outputName]
+            return np.array(log[self._outputName])
     
     def simulate(self, parameters, times, continueOnError = True, incrementSolveCounter = True):
         """

ionbench/problems/moreno2016.py
@@ -229,7 +229,7 @@ class ina(ionbench.benchmarker.Benchmarker):
                 return self.run_moreno()
             except:
                 warnings.warn("Failed to solve model. Will report infinite output in the hope of continuing the run.")
-                return [np.inf]*69
+                return np.array([np.inf]*69)
         else:
             return self.run_moreno()
     
@@ -304,7 +304,7 @@ class ina(ionbench.benchmarker.Benchmarker):
                 if reachedPeak and abs(current)<=abs(peak/2):
                     tau[i] = self._logTimes[self._tauBounds[i][0]+j]-timeToPeak
                     break
-        return ssi+act+rfi+rudb+tau
+        return np.array(ssi+act+rfi+rudb+tau)
 
 def generate_data():
     """