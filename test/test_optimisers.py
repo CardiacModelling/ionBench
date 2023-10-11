0d58d62dc579008d4d9b3bcc0d9c6bc3099e4ec4
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-10-11 14:35:45 +0100

    Added x0 to external optimiser tests
    Initial conditions are now included in the external optimiser unit tests. We don't assert a reduction in cost after optimising like we do with the other optimisers since x0 is perturbed to produce the populations used by these optimisers, so the point x0 might not actually be used

--- test/test_optimisers.py
+++ test/test_optimisers.py
@@ -41,10 +41,6 @@ class Test_external:
     optimisers = [(ext_opts.GA_Bot2012.run, {'nGens':5}), (ext_opts.GA_Smirnov2020.run, {'nGens':5}), (ext_opts.hybridPSOTRR_Loewe2016.run, {'n':5, 'Lmax':5}), (ext_opts.patternSearch_Kohjitani2022.run, {'maxfev':50}), (ext_opts.ppso_Chen2012.run, {'n':5,'lmax':5})]
     @pytest.mark.parametrize("run", optimisers)
     def test_cost_reduction(self, run):
-        #x0 = self.bm.sample()
-        #cost = self.bm.cost(x0)
-        #sample initial point, find its cost, optimise, find opt cost, assert reduction
-        x0_opt = run[0](self.bm, **run[1])
-        #cost_opt = self.bm.cost(x0_opt)
-        self.bm.reset()
-        #assert cost_opt <= cost
+        x0 = self.bm.sample()
+        #sample initial point, find its cost, optimise, find opt cost, can't assert reduction - population algorithms sample around x0 not at x0
+        x0_opt = run[0](self.bm, x0 = x0, **run[1])
