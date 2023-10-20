b948ea02c0b73bc3f02f187ad8fecd77209b65cd
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-10-20 14:05:02 +0100

    Began simulated annealing Vanier approach
    This is the first part of implementing this approach. A standard nelder mead implementation has been made that will then be edited to match Press "Numerical Recipes in C" and then changed again to match the final simulated annealing algorithm. This nelder mead implementation has been checked against (and matches) the scipy implementation

--- ionbench/optimisers/external_optimisers/__init__.py
+++ ionbench/optimisers/external_optimisers/__init__.py
@@ -3,3 +3,4 @@ from ionbench.optimisers.external_optimisers import GA_Smirnov2020
 from ionbench.optimisers.external_optimisers import patternSearch_Kohjitani2022
 from ionbench.optimisers.external_optimisers import ppso_Chen2012
 from ionbench.optimisers.external_optimisers import hybridPSOTRR_Loewe2016
+from ionbench.optimisers.external_optimisers import SA_Vanier1999
