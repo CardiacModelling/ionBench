f5f93addc9d466451e67017795a0c300d70ef214
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-11-01 14:10:03 +0000

    Update to SA Vanier 1999 comments
    Clarified some more details on how this implementation was built

--- ionbench/optimisers/external_optimisers/SA_Vanier1999.py
+++ ionbench/optimisers/external_optimisers/SA_Vanier1999.py
@@ -1,4 +1,4 @@
-# This code was first implemented as a standard nelder mead, verified against scipy nelder mead, then adjusted to reproduce the simulated annealing of "Numerical Recipes in C"
+# This code was first implemented as a standard nelder mead (Adapted from Scholorpedia http://dx.doi.org/10.4249/scholarpedia.2928) which was then checked against the amoeba algorithm in "Numerical Recipes in C" by Press et al. The resulting Nelder Mead algorithm was verified against scipy nelder mead, then adjusted to reproduce the simulated annealing of "Numerical Recipes in C" by perturbing the costs.
 
 import numpy as np
 import ionbench
@@ -6,7 +6,7 @@ import ionbench
 
 def run(bm, x0=[], tempInitial=None, N=5, maxIter=100, debug=False):
     """
-    Simulated Annealing by Vanier 1999 uses the algorithm presented in "Numerical Recipes in C" by Press. This features a nelder mead optimiser, in which each point in the simplex, in addition to the cost also stored a realisation of a logarithmically distributed random variable. When points are compared, if the point is already in the simplex, this random variable is added to the cost, and if the point is not yet in the simplex (or was added during this step), then it is subtracted from the cost. This ensures that 1) the point which is moved (the worst point in nelder mead) is now random, and 2) there is a non-zero probability to accept a move that increases the cost. As the temperature decreases, the magnitude of this noise also decreases, ensuring the algorithm approaches a standard nelder mead simplex as the number of iterations approaches the maximum number.
+    Simulated Annealing by Vanier 1999 uses the algorithm presented in "Numerical Recipes in C" by Press et al. This features a nelder mead optimiser, in which each point in the simplex, in addition to the cost also stored a realisation of a logarithmically distributed random variable. When points are compared, if the point is already in the simplex, this random variable is added to the cost, and if the point is not yet in the simplex (or was added during this step), then it is subtracted from the cost. This ensures that 1) the point which is moved (the worst point in nelder mead) is now random, and 2) there is a non-zero probability to accept a move that increases the cost. As the temperature decreases, the magnitude of this noise also decreases, ensuring the algorithm approaches a standard nelder mead simplex as the number of iterations approaches the maximum number.
     Parameters
     ----------
     bm : Benchmarker
