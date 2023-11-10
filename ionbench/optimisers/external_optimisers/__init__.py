262e071fb54215790e1b54682bcf741e1686161f
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-11-10 09:46:25 +0000

    added cairns 2017 ga approach

--- ionbench/optimisers/external_optimisers/__init__.py
+++ ionbench/optimisers/external_optimisers/__init__.py
@@ -6,3 +6,4 @@ from ionbench.optimisers.external_optimisers import hybridPSOTRR_Loewe2016
 from ionbench.optimisers.external_optimisers import SA_Vanier1999
 from ionbench.optimisers.external_optimisers import SPSA_Spall1998
 from ionbench.optimisers.external_optimisers import curvilinearGD_Dokos2004
+from ionbench.optimisers.external_optimisers import GA_Cairns2017
