82386113c4964c88b58c427e11c945d8ac964ef8
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-11-15 11:59:24 +0000

    added list of all optimisers and approaches

--- test/runOptimisers.py
+++ test/runOptimisers.py
@@ -1,27 +1,13 @@
 import ionbench
 import importlib
 
-basePath = 'ionbench.optimisers.'
-scipyPath = 'scipy_optimisers.'
-externalPath = 'external_optimisers.'
-pintsPath = 'pints_optimisers.'
-tmp = ['lm', 'nelderMead', 'powell', 'trustRegionReflective']
-scipyOpt = [scipyPath + s + '_scipy' for s in tmp]
-tmp = ['cmaes', 'nelderMead', 'pso', 'snes', 'xnes']
-pintsOpt = [pintsPath + s + '_pints' for s in tmp]
-tmp = ['GA_Bot2012', 'GA_Smirnov2020', 'hybridPSOTRR_Loewe2016', 'patternSearch_Kohjitani2022', 'ppso_Chen2012']
-extOpt = [externalPath + s for s in tmp]
-
-opts = [basePath + s for s in (scipyOpt + pintsOpt + extOpt)]
-print(opts)
 bm = ionbench.problems.staircase.HH_Benchmarker()
-
 initParams = bm.sample(n=5)
 
-for st in opts:
-    print(st)
-    opt = importlib.import_module(st)
-    mod = opt.get_modification()
+for app in ionbench.APP_ALL:
+    print(app['module'])
+    opt = importlib.import_module(app['module'])
+    mod = opt.get_modification(app['modNum'])
     mod.apply(bm)
-    ionbench.multistart(opt.run, bm, initParams, st)
+    ionbench.multistart(opt.run, bm, initParams, app['module'] + 'modNum' + str(app['modNum']), **app['kwargs'])
     bm.reset()
