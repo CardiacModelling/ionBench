bug fix: sign error in tournement selection

ionbench/optimisers/external_optimisers/GA_Gurkiewicz2007b.py
@@ -71,7 +71,7 @@ def run(bm, x0=[], nGens=1000, popSize=0, debug=False):
         for j in range(2):
             perm = np.random.permutation(popSize)
             for i in range(popSize // 2):
-                if pop[perm[2 * i]].cost > pop[perm[2 * i + 1]].cost:
+                if pop[perm[2 * i]].cost < pop[perm[2 * i + 1]].cost:
                     newPop.append(copy.deepcopy(pop[perm[2 * i]]))
                 else:
                     newPop.append(copy.deepcopy(pop[perm[2 * i + 1]]))