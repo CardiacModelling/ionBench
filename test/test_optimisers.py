d573866ac9a2906b0f26e78400f7ca2d994ad8da
Matt J Owen <matt.owen@nottingham.ac.uk>
2023-11-23 17:07:07 +0000

    added optimiser tests using new "test" problem

--- test/test_optimisers.py
+++ test/test_optimisers.py
@@ -15,6 +15,8 @@ class Test_scipy:
     bm._useScaleFactors = True  # Helps the stability of conjugate GD (avoiding big steps which lead to parameters that result in failed model solves)
     bm.plotter = False
 
+    bmTest = ionbench.problems.test.Test()
+
     @pytest.mark.parametrize("opt", ionbench.OPT_SCIPY)
     @pytest.mark.filterwarnings("ignore::RuntimeWarning")
     def test_cost_reduction(self, opt):
@@ -27,11 +29,21 @@ class Test_scipy:
         self.bm.reset()
         assert cost_opt <= cost
 
+    @pytest.mark.parametrize("opt", ionbench.OPT_SCIPY)
+    @pytest.mark.filterwarnings("ignore::RuntimeWarning")
+    def test_minimum_finding(self, opt):
+        module = import_module(opt)
+        x0_opt = module.run(self.bmTest)
+        cost_opt = self.bmTest.cost(x0_opt)
+        assert cost_opt < 5e-3
+
 
 class Test_pints:
     # Check all pints optimisers run and improve cost compared to x0
     bm = ionbench.problems.staircase.HH_Benchmarker()
     bm.plotter = False
+
+    bmTest = ionbench.problems.test.Test()
 
     @pytest.mark.parametrize("opt", ionbench.OPT_PINTS)
     @pytest.mark.filterwarnings("ignore::RuntimeWarning")
@@ -45,12 +57,22 @@ class Test_pints:
         self.bm.reset()
         assert cost_opt <= cost
 
+    @pytest.mark.parametrize("opt", ionbench.OPT_PINTS)
+    @pytest.mark.filterwarnings("ignore::RuntimeWarning")
+    def test_minimum_finding(self, opt):
+        module = import_module(opt)
+        x0_opt = module.run(self.bmTest)
+        cost_opt = self.bmTest.cost(x0_opt)
+        assert cost_opt < 5e-3
+
 
 class Test_external:
     # Check all external optimisers run
     bm = ionbench.problems.staircase.HH_Benchmarker(sensitivities=True)
     bm._useScaleFactors = True
     bm.plotter = False
+
+    bmTest = ionbench.problems.test.Test()
 
     @pytest.mark.parametrize("opt", ionbench.OPT_EXT)
     @pytest.mark.filterwarnings("ignore::RuntimeWarning")
@@ -63,3 +85,16 @@ class Test_external:
         cost_opt = self.bm.cost(x0_opt)
         self.bm.reset()
         assert cost_opt < np.inf
+
+    @pytest.mark.parametrize("opt", ionbench.OPT_EXT)
+    @pytest.mark.filterwarnings("ignore::RuntimeWarning")
+    def test_minimum_finding(self, opt):
+        module = import_module(opt)
+        if 'Loewe' in opt:
+            x0_opt = module.run(self.bmTest, maxIter=50)
+        elif 'Liu' in opt or 'Chen' in opt or 'Cabo' in opt:
+            x0_opt = module.run(self.bmTest, gmin=5e-3)
+        else:
+            x0_opt = module.run(self.bmTest)
+        cost_opt = self.bmTest.cost(x0_opt)
+        assert cost_opt < 5e-3
