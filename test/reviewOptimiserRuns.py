2f71af2f10e3dc1a389da4032a6a8f9594411199
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-01-04 11:09:28 +0000

    improvements to functions for running and reviewing optimisers
    Running optimisers:
    Bug fix - Initial conditions should be specified in input parameter space
    Stability - Added try except to catch any potential crashes/errors
    
    Reviewing optimisers:
    Added automation for reviewing optimiser outputs. All results are extracted from the tracker pickles and saved to a csv

--- test/reviewOptimiserRuns.py
+++ test/reviewOptimiserRuns.py
@@ -1,23 +1,79 @@
 import ionbench
-import matplotlib.pyplot as plt
+import numpy as np
+import csv
 
-basePath = 'ionbench.optimisers.'
-scipyPath = 'scipy_optimisers.'
-externalPath = 'external_optimisers.'
-pintsPath = 'pints_optimisers.'
-tmp = ['lm', 'nelderMead', 'powell', 'trustRegionReflective']
-scipyOpt = [scipyPath + s + '_scipy' for s in tmp]
-tmp = ['cmaes', 'nelderMead', 'pso', 'snes', 'xnes']
-pintsOpt = [pintsPath + s + '_pints' for s in tmp]
-tmp = ['GA_Bot2012', 'GA_Smirnov2020', 'patternSearch_Kohjitani2022', 'ppso_Chen2012']
-extOpt = [externalPath + s for s in tmp]
-
-opts = [basePath + s for s in (scipyOpt + pintsOpt + extOpt)]
-print(opts)
 bm = ionbench.problems.staircase.HH_Benchmarker()
-
-for st in opts:
-    bm.tracker.load('tracker_' + st + '.pickle')
-    plt.figure()
-    plt.title(st)
-    bm.tracker.plot()
+with open('resultsFile.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    titles = ['Optimiser Name', 'Mod Num']
+    variables = ['Conv Cost', 'Best Cost', 'Cost FE', 'Grad FE', 'Parameters Identified', 'Average Cost FE Time', 'Average Grad FE Time']
+    for i in range(5):
+        for j in variables:
+            titles.append(f'Run {i} - {j}')
+    titles += ['Success Rate', 'Expected Cost FE', 'Expected Grad FE', 'Tier', 'Tier Score']
+    writer.writerow(titles)
+    for app in ionbench.APP_ALL:
+        print(app['module'])
+        costAtConv = []
+        bestCost = []
+        costEvalsAtConv = []
+        gradEvalsAtConv = []
+        paramIdenAtConv = []
+        costAverTime = []
+        gradAverTime = []
+        try:
+            bm.tracker.load(f"{app['module']}modNum{app['modNum']}_run0.pickle")
+        except Exception as e:
+            print(e)
+            print(f"Tracking of {app['module']} failed. Are you sure that the .pickle file of results exists?")
+            optimiserName = app['module'].split('.')[-1]
+            modNum = app['modNum']
+            row = [optimiserName, modNum]
+            row += [np.nan]*(len(titles)-2)
+            writer.writerow(row)
+            continue
+        for runNum in range(5):
+            bm.tracker.load(f"{app['module']}modNum{app['modNum']}_run{runNum}.pickle")
+            costAverTime.append(np.mean(bm.tracker.costTimes))
+            gradAverTime.append(np.mean(bm.tracker.gradTimes))
+            bestCost.append(bm.tracker.bestCost)
+            finalParamId = bm.tracker.paramIdentifiedCount[-1]
+            paramIdenAtConv.append(finalParamId)
+            ifEqualFinalParamId = bm.tracker.paramIdentifiedCount == finalParamId
+            ind = [i for i, x in enumerate(ifEqualFinalParamId) if x]  # Indexes where number of parameters identified is equal to the final count
+            for i in ind:
+                if all(ifEqualFinalParamId[i:]):
+                    # All future points remain with this many parameters identified, therefore it is considered converged
+                    costEvalsAtConv.append(bm.tracker.modelSolves[i])
+                    gradEvalsAtConv.append(bm.tracker.gradSolves[i])
+                    costAtConv.append(bm.tracker.costs[i])
+                    break
+        
+            bm.reset()
+        successRate = np.mean(np.array(paramIdenAtConv)==bm.n_parameters())
+        if successRate == 1:
+            expectedCostEvals = np.mean(costEvalsAtConv)
+            expectedGradEvals = np.mean(gradEvalsAtConv)
+            tier = 1
+            score = expectedCostEvals + expectedGradEvals*gradAverTime/costAverTime
+        elif successRate > 0:
+            Tsucc = np.mean([costEvalsAtConv[t] for t in range(5) if paramIdenAtConv[t]==bm.n_parameters()])
+            Tfail = np.mean([costEvalsAtConv[t] for t in range(5) if paramIdenAtConv[t]!=bm.n_parameters()])
+            expectedCostEvals = Tsucc + Tfail*(1-successRate)/successRate
+            Tsucc = np.mean([gradEvalsAtConv[t] for t in range(5) if paramIdenAtConv[t]==bm.n_parameters()])
+            Tfail = np.mean([gradEvalsAtConv[t] for t in range(5) if paramIdenAtConv[t]!=bm.n_parameters()])
+            expectedGradEvals = Tsucc + Tfail*(1-successRate)/successRate
+            tier = 2
+            score = expectedCostEvals + expectedGradEvals*gradAverTime/costAverTime
+        else:
+            tier = 3
+            expectedCostEvals = np.inf
+            expectedGradEvals = np.inf
+            score = np.mean(costEvalsAtConv+gradEvalsAtConv*gradAverTime/costAverTime)/np.mean(paramIdenAtConv)
+        optimiserName = app['module'].split('.')[-1]
+        modNum = app['modNum']
+        row = [optimiserName, modNum]
+        for i in range(5):
+            row += [costAtConv[i], bestCost[i], costEvalsAtConv[i], gradEvalsAtConv[i], paramIdenAtConv[i], costAverTime[i], gradAverTime[i]]
+        row += [successRate, expectedCostEvals, expectedGradEvals, tier, score]
+        writer.writerow(row)
--- test/runOptimisers.py
+++ test/runOptimisers.py
@@ -3,12 +3,17 @@ import importlib
 
 bm = ionbench.problems.staircase.HH_Benchmarker()
 initParams = bm.sample(n=5)
-
+print(initParams)
 for app in ionbench.APP_ALL:
     print(app['module'])
-    opt = importlib.import_module(app['module'])
-    mod = opt.get_modification(app['modNum'])
-    mod.apply(bm)
-    kwargs = app['kwargs'] | mod.kwargs
-    ionbench.multistart(opt.run, bm, initParams, app['module'] + 'modNum' + str(app['modNum']), **kwargs)
+    try:
+        opt = importlib.import_module(app['module'])
+        mod = opt.get_modification(app['modNum'])
+        mod.apply(bm)
+        x0 = [bm.input_parameter_space(x) for x in initParams]
+        kwargs = app['kwargs'] | mod.kwargs
+        ionbench.multistart(opt.run, bm, x0, app['module'] + 'modNum' + str(app['modNum']), **kwargs)
+    except Exception as e:
+        print(e)
+        print(f"Approach {app['module']} with modNum {app['modNum']} failed. Will skip and keep going to get as many results as possible")
     bm.reset()
