0fdeab05e68f1a61ea31ece59b69f0a5bdf943a2
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-04-22 15:51:42 +0100

    added classes_pints to utils' init import

--- ionbench/utils/__init__.py
+++ ionbench/utils/__init__.py
@@ -4,3 +4,4 @@ from ionbench.utils import population_optimisers
 from ionbench.utils import cache
 from ionbench.utils import scipy_setup
 from ionbench.utils import particle_optimisers
+from ionbench.utils import classes_pints
