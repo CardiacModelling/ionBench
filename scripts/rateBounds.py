b157ca3b23adba4df2f131cca20530d5345c9ba9
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-07-16 11:22:42 +0100

    simplified rate bounds script
    removed some excess figures

--- scripts/rateBounds.py
+++ scripts/rateBounds.py
@@ -26,23 +26,6 @@ def staircase_plots(bm, paramPairs, nPoints):
         plt.title(f'Rate bounds - {modelType} Staircase - ($p_{{{i}}}$,$p_{{{j}}}$)')
         plt.savefig(f'figures/rateBounds-{modelType}-{i}.png')
         plt.close()
-        # Create a contour plot of the penalty function at multiple levels
-        x = np.logspace(np.log10(bm._LOWER_BOUND[i] * 0.01), np.log10(bm._UPPER_BOUND[i] * 10), nPoints)
-        y = np.linspace(-0.2, bm._UPPER_BOUND[j] * 1.75, nPoints)
-        x, y = np.meshgrid(x, y)
-        z = vecF(x, y, i, j)
-        CS = plt.contour(x, y, z, [1e5, 2e5, 5e5, 1e6])
-        plt.gca().set_xscale('log')
-        plt.title(f'Penalty function - {modelType} Staircase - ($p_{{{i}}}$,$p_{{{j}}}$)')
-        plt.clabel(CS, inline=1, fontsize=10)
-        plt.scatter(bm._TRUE_PARAMETERS[i], bm._TRUE_PARAMETERS[j], marker='*')
-        plt.savefig(f'figures/penalty-{modelType}-' + str(i) + '.png')
-        plt.close()
-        # Create a heatmap of the penalty function
-        plt.pcolor(x, y, z, norm=mpl.colors.LogNorm(vmin=1e5, vmax=z.max()))
-        plt.gca().set_xscale('log')
-        plt.savefig(f'figures/penaltyHeatmap-{modelType}-' + str(i) + '.png')
-        plt.close()
         print(f'Parameter pair {i},{j} complete')
 
 
@@ -162,8 +145,8 @@ def paper_plot():
 
 paper_plot()
 
-#bm = ionbench.problems.staircase.HH()
-#staircase_plots(bm, [(0, 1), (2, 3), (4, 5), (6, 7)], nPoints=1000)
+bm = ionbench.problems.staircase.HH()
+staircase_plots(bm, [(0, 1), (2, 3), (4, 5), (6, 7)], nPoints=1000)
 
-#bm = ionbench.problems.staircase.MM()
-#staircase_plots(bm, [(0, 1), (2, 14), (3, 4), (5, 6), (7, 8), (10, 11), (12, 13), (9, 14)], nPoints=1000)
+bm = ionbench.problems.staircase.MM()
+staircase_plots(bm, [(0, 1), (2, 14), (3, 4), (5, 6), (7, 8), (10, 11), (12, 13), (9, 14)], nPoints=1000)
