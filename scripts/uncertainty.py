b3fa84d0741bbb12a23e970f22defc662b7cd878
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-08-08 14:15:47 +0100

    added profile likelihood results
    added pickle files to repo
    updated scripts to generate the files (should match what was run on the HPC, but with more useful file save locations)

--- scripts/uncertainty.py
+++ scripts/uncertainty.py
@@ -1,30 +1,35 @@
 import ionbench
 import numpy as np
+import os
+
+def var(n):
+    return np.linspace(0.8, 1.2, n)
 
 
-# %% Profile likelihood
-def var(x):
-    return np.linspace(1 - x, 1 + x, 51)
-
+script_path = os.path.abspath(os.path.dirname(__file__))
+extension = os.path.join(script_path, '..', 'results', 'uncertainty')
 
 bm = ionbench.problems.staircase.HH()
-variations = [var(0.2)] * bm.n_parameters()
-ionbench.uncertainty.profile_likelihood.run(bm, variations, filename='hh')
-ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename='hh')
+variations = [var(51)] * bm.n_parameters()
+ionbench.uncertainty.profile_likelihood.run(bm, variations, filename=extension+'/hh')
+ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename=extension+'/hh')
 
 bm = ionbench.problems.staircase.MM()
-variations = [var(0.2)] * bm.n_parameters()
-ionbench.uncertainty.profile_likelihood.run(bm, variations, filename='mm')
-ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename='mm')
+variations = [var(51)] * bm.n_parameters()
+ionbench.uncertainty.profile_likelihood.run(bm, variations, filename=extension+'/mm')
+ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename=extension+'/mm')
 
 bm = ionbench.problems.loewe2016.IKr()
-variations = [var(0.2)] * bm.n_parameters()
-ionbench.uncertainty.profile_likelihood.run(bm, variations, filename='ikr')
-ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename='ikr')
+variations = [var(51)] * bm.n_parameters()
+ionbench.uncertainty.profile_likelihood.run(bm, variations, filename=extension+'/ikr')
+ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename=extension+'/ikr')
 
 bm = ionbench.problems.loewe2016.IKur()
-variations = [var(0.2)] * bm.n_parameters()
-ionbench.uncertainty.profile_likelihood.run(bm, variations, filename='ikur')
-ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename='ikur')
+variations = [var(51)] * bm.n_parameters()
+ionbench.uncertainty.profile_likelihood.run(bm, variations, filename=extension+'/ikur')
+ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename=extension+'/ikur')
 
-
+bm = ionbench.problems.moreno2016.INa()
+variations = [var(1005) if i in [0, 1, 5, 8, 9, 10, 11, 12, 13, 14] else var(103) for i in range(bm.n_parameters())]
+ionbench.uncertainty.profile_likelihood.run(bm, variations, filename=extension+'/ina')
+ionbench.uncertainty.profile_likelihood.run(bm, variations, backwardPass=True, filename=extension+'/ina')
