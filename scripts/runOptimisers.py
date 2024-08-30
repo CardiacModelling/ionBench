4659e9e6b159d73e5e6761dfb19712b798a46757
Matt J Owen <matt.owen@nottingham.ac.uk>
2024-08-30 15:33:04 +0100

    corrected run optimisers to match hpc
    the scripts that were actually used were on the hpc, this version hadn't been updated in a while
    main differences are:
    unraveled for loop for batch/parallel processing
    setting maxiter and nGens
    number of sampled parameters

--- scripts/runOptimisers.py
+++ scripts/runOptimisers.py
@@ -1,21 +1,26 @@
 import ionbench
 import importlib
+from inspect import signature
 import numpy as np
+import os
+
+np.random.seed(0)
+
+batch = int(os.environ['SLURM_ARRAY_TASK_ID'])
 
 bm = ionbench.problems.staircase.HH()
-np.random.seed(0)
-initParams = bm.sample(n=5)
+bm.plotter = False
+initParams = bm.sample(10)
 print(initParams)
-for app in ionbench.APP_UNIQUE:
-    print(app['module'])
-    try:
-        opt = importlib.import_module(app['module'])
-        mod = opt.get_modification(app['modNum'])
-        mod.apply(bm)
-        x0 = [bm.input_parameter_space(x) for x in initParams]
-        kwargs = app['kwargs'] | mod.kwargs
-        ionbench.utils.multistart(opt.run, bm, x0, app['module'] + 'modNum' + str(app['modNum']), **kwargs)
-    except Exception as e:
-        print(e)
-        print(f"Approach {app['module']} with modNum {app['modNum']} failed. Will skip and keep going to get as many results as possible")
-    bm.reset()
+app = ionbench.APP_UNIQUE[batch]
+print(app['module'])
+opt = importlib.import_module(app['module'])
+mod = opt.get_modification(app['modNum'])
+mod.apply(bm)
+x0 = [bm.input_parameter_space(x) for x in initParams]
+kwargs = app['kwargs'] | mod.kwargs
+if 'maxIter' in signature(opt.run).parameters:
+    kwargs = kwargs | {'maxIter': 25000}
+if 'nGens' in signature(opt.run).parameters:
+    kwargs = kwargs | {'nGens': 10000}
+ionbench.utils.multistart(opt.run, bm, x0, 'hh_' + app['module'] + 'modNum' + str(app['modNum']), **kwargs)
